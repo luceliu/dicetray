fecdb42605fec169795840138263eece9f889a0a
Luce <lucy_liu@live.ca>
2018-05-26 15:29:30 -0400

    console-log-implemented rolling for all dice

--- public/scripts/dice.js
+++ public/scripts/dice.js
@@ -1,2 +1,14 @@
 // alert('dice connected');
-console.log('fuck');
+$(document).ready(function() {
+    $(".btn-roll-dice").click(function () {
+        let roll = $(this).parent()[0].id.substring(1);
+        roll = Number(roll);
+        console.log(roll);
+        console.log("Rolled a " + getRandomNumberUpTo(roll));
+    });
+
+});
+
+function getRandomNumberUpTo(number) {
+    return Math.floor((Math.random() * number) + 1);
+}
