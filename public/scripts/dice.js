b4f7c15570c4bd7dd7907548ffa9164b5a927fe7
Luce <lucy_liu@live.ca>
2018-05-26 18:19:45 -0400

    added h:mm:ss timestamp to logs

--- public/scripts/dice.js
+++ public/scripts/dice.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $(".btn-roll-dice").click(function () {
         let roll = $(this).parent()[0].id.substring(1);
         roll = Number(roll);
+        console.log("Date: "+ Date.now());
         // console.log("Rolling a d"+roll);
         let numTimes = $(this).siblings('input').val();
         console.log("numTimes: "+numTimes);
@@ -43,7 +44,7 @@ function rollDice(d, times) {
         // $("#roll-history").append(getRandomNumberUpTo(d) +'\n');
     }
     // console.log('times: '+times);
-    $("#roll-history").append("Rolled a d" + d +" and got: ");
+    $("#roll-history").append("["+timeStamp()+"] Rolled a d" + d +" and got: ");
     for (i = 0; i < results.length; i++) {
         if (i === results.length-1) {
             $("#roll-history").append(results[i]+".");
@@ -57,4 +58,33 @@ function rollDice(d, times) {
 
 function getRandomNumberUpTo(number) {
     return Math.floor((Math.random() * number) + 1);
+}
+function timeStamp() {
+// Create a date object with the current time
+    var now = new Date();
+
+// Create an array with the current month, day and time
+//     var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+
+// Create an array with the current hour, minute and second
+    var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+
+// Determine AM or PM suffix based on the hour
+    var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+
+// Convert hour from military time
+    time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+
+// If hour is 0, set it to 12
+    time[0] = time[0] || 12;
+
+// If seconds and minutes are less than 10, add a zero
+    for ( var i = 1; i < 3; i++ ) {
+        if ( time[i] < 10 ) {
+            time[i] = "0" + time[i];
+        }
+    }
+
+// Return the formatted string
+    return time.join(":") + " " + suffix;
 }
